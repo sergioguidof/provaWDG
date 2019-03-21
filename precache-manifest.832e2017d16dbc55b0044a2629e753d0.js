8bbee668ce66b9a775f40a554f72887023571a1f
Sergio <sergio.guidof@gmail.com>
2019-03-20 22:35:45 -0300

    Updates

---
+++ precache-manifest.832e2017d16dbc55b0044a2629e753d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/provaWDG/static/media/react.580ffd26.png"
+  },
+  {
+    "revision": "bfe729bd8ff22c7b765a",
+    "url": "/provaWDG/static/js/runtime~main.e13b917e.js"
+  },
+  {
+    "revision": "3d9c2098423851ac2c44",
+    "url": "/provaWDG/static/js/main.75dc20eb.chunk.js"
+  },
+  {
+    "revision": "d52dd1da8dd654981181",
+    "url": "/provaWDG/static/js/2.75a0ca17.chunk.js"
+  },
+  {
+    "revision": "3d9c2098423851ac2c44",
+    "url": "/provaWDG/static/css/main.e77cc633.chunk.css"
+  },
+  {
+    "revision": "d52dd1da8dd654981181",
+    "url": "/provaWDG/static/css/2.150eafcf.chunk.css"
+  },
+  {
+    "revision": "75de5987525958ed2658e639fd1c691c",
+    "url": "/provaWDG/index.html"
+  }
+];
